c704e1d6d420acd875f3736db6e99ba19a526be0
almahmoud <amahmou4@jhu.edu>
2020-02-05 13:17:55 -0500

    Better packaging script including galaxy

--- package_all.sh
+++ package_all.sh
@@ -1,1 +1,44 @@
-rm -rf ../cloudlaunch-helm/cloudlaunchserver/charts && rm -rf ../cloudlaunch-helm/cloudlaunch/charts && rm -rf ../cloudman-helm/cloudman/charts && cd ../cloudlaunch-helm/cloudlaunchserver && helm dependency update && cd ../../cloudman-helm/cloudman && helm dependency update && cd ../../helm-charts/charts && helm package ../../cloudlaunch-helm/cloudlaunchserver/ && mkdir -p ../../cloudlaunch-helm/cloudlaunch/charts && cp cloudlaunchserver-0.3.0.tgz ../../cloudlaunch-helm/cloudlaunch/charts/ && helm package ../../cloudlaunch-helm/cloudlaunch && cp cloudlaunch-0.3.0.tgz ../../cloudman-helm/cloudman/charts/ && helm package ../../cloudman-helm/cloudman/ && cd .. && rm -f index.yaml && helm repo index . --url https://raw.githubusercontent.com/cloudve/helm-charts/master/ && git add .
+# /bin/sh
+
+# This script MUST be run from within helm-charts directory
+# This script assumes the following directories exist:
+# ../cloudman-helm
+# ../cloudlaunch-helm
+# ../galaxy-helm
+# This script will package cloudlaunch, cloudlaunchserver, cloudman, and galaxy
+# and ignore outside dependencies for all charts except 
+
+REPO_NAME=cloudve
+BRANCH_NAME=master
+
+rm -rf ../cloudlaunch-helm/cloudlaunchserver/charts
+rm -rf ../cloudlaunch-helm/cloudlaunch/charts
+rm -rf ../cloudman-helm/cloudman/charts
+rm -rf ../galaxy-helm/galaxy/charts
+rm -rf ../cloudlaunch-helm/cloudlaunchserver/tmpcharts
+rm -rf ../cloudlaunch-helm/cloudlaunch/tmpcharts
+rm -rf ../cloudman-helm/cloudman/tmpcharts
+rm -rf ../galaxy-helm/galaxy/tmpcharts
+cd ../cloudlaunch-helm/cloudlaunchserver
+helm dependency update
+cd ../../cloudman-helm/cloudman
+helm dependency update
+cd ../../helm-charts/charts
+echo "\nPackaging cloudlaunchserver!\n"
+helm package ../../cloudlaunch-helm/cloudlaunchserver/
+mkdir -p ../../cloudlaunch-helm/cloudlaunch/charts
+cp cloudlaunchserver-0.3.0.tgz ../../cloudlaunch-helm/cloudlaunch/charts/
+echo "\nPackaging cloudlaunch!\n"
+helm package ../../cloudlaunch-helm/cloudlaunch
+cp cloudlaunch-0.3.0.tgz ../../cloudman-helm/cloudman/charts/
+echo "\nPackaging cloudman!\n"
+helm package ../../cloudman-helm/cloudman/
+export CHARTS_DIR=$(pwd)
+cd ../../galaxy-helm/galaxy
+helm dependency update
+echo "\nPackaging galaxy!\n"
+sh scripts/helm_package $CHARTS_DIR
+cd ../../helm-charts
+rm -f index.yaml
+echo "\nReindexing!\n"
+helm repo index . --url https://raw.githubusercontent.com/$REPO_NAME/helm-charts/$BRANCH_NAME/
