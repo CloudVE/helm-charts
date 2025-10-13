547c80105424097e8a9771d668fcab29a146bbbf
Keith Suderman <suderman@jhu.edu>
2025-10-13 10:52:44 -0400

    Manually releasing galaxy-deps chart

---
+++ index.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eu
+
+REPO=cloudve
+BRANCH=$(git branch --show-current)
+
+while [[ $# -gt 0 ]] ; do
+	case $1 in
+		-r|--repo) REPO=$2 ; shift ;;
+		-b|--branch) BRANCH=$2 ; shift ;;
+		-h|--help|help)
+			echo "$(basename $(realpath $0)) [-b|--branch BRANCH] [-r|--repo REPO] "
+			exit 0
+			;;
+		*) 
+			echo "Invalid option $1"
+			exit 1;
+			;;
+	esac
+	shift
+done
+echo "Generating the Helm index for the $BRANCH branch in the $REPO repository"
+helm repo index . --url https://raw.githubusercontent.com/$REPO/helm-charts/$BRANCH/
